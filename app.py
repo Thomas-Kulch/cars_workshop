4dba1c737be7981ad5983a2b043e584d3c79c334
Thomas-Kulch <54332517+Thomas-Kulch@users.noreply.github.com>
2024-02-20 21:18:58 -0500

    Added figures and cleaned up code

--- app.py
+++ app.py
@@ -5,20 +5,22 @@ import streamlit as st
 
 import variables as v
 
-#App headers
+#Inventory Filter
 st.header('Market of Used Cars')
-st.write('Browse our inventory of used cars. \n\n Narrow your selection by filtering by Manufacturer and Model Year. And no, you can\'t see the photos.')
+st.write('Browse our inventory of used car. \n\n Narrow your selection by filtering by Manufacturer and Model Year. And no, you can\'t see the photos.')
 
 #Selection box for manufacturers
 selection = st.selectbox('Select a Manufacturer', v.unique_manufacturer)
 
 
-#Slider
+#Slider range
 year_range = st.slider('Year Filter',value=(v.min_year,v.max_year),min_value=v.min_year,max_value=v.max_year)
 actual_range = list(range(year_range[0],year_range[1]+1))
 
+#Adding the selection box and slider outcomes to the results of the dataframe.
 car_filtered = v.car_ads[(v.car_ads.manufacturer_name == selection) & (v.car_ads.year_produced.isin(actual_range))]
 
+#renaming dataframe columns to clean it up on the app
 car_filtered.rename(columns={'manufacturer_name':'Manufacturer Name',
                              'model_name':'Model Name',
                              'transmission':'Transmission',
@@ -34,9 +36,39 @@ car_filtered.rename(columns={'manufacturer_name':'Manufacturer Name',
                              'number_of_photos':'No. of Photos'},inplace=True)
 
 
-
-
+#print the inventory table on the app with formatting
 st.dataframe(car_filtered.set_index(car_filtered.columns[0]).style.format({'Odometer Value':'{:,}',
                                                                            'Year Produced':'{}',
                                                                            'Engine Capacity':'{:.1f}',
                                                                            'Price (USD)':'{:,.2f}'}))
+
+
+#New section of the page: Price Analysis
+st.header('Price Analysis')
+st.write("""
+###### Let's analyze what influences price the most. We will check how distribution of price varies depending on transmission, engine, body type or state.
+""")
+
+#create a list for our drop down and then add the dropdown
+list_for_hist = ['transmission','engine_type','body_type','state']
+price_menu = st.selectbox('Select a Category',list_for_hist)
+
+fig1 = px.histogram(v.car_ads,x='price_usd',color=price_menu)
+fig1.update_layout(title='<b> Split of Price by {}'.format(price_menu))
+
+st.plotly_chart(fig1)
+
+
+#New report: Price Dependency
+st.write("""
+###### Now let's check how price is affected by things like odometer reading, engine capacity or number of photos in the ads
+""")
+
+#create a list for our drop down and then add the dropdown
+list_for_scatter = ['odometer_value','engine_capacity','number_of_photos']
+scatter_select = st.selectbox('Price dependency on:',list_for_scatter)
+
+
+fig2 = px.scatter(v.car_ads,x='price_usd',y=scatter_select,color=v.car_ads['age_category'])
+
+st.plotly_chart(fig2)
--- variables.py
+++ variables.py
@@ -7,8 +7,26 @@ car_ads = pd.read_csv('cars_workshop.csv')
 #drop unwanted column
 car_ads = car_ads.drop(car_ads.columns[0],axis=1)
 
+#add age column
+car_ads['age'] = 2024 - car_ads['year_produced']
+
 #get distinct car manufacturers and store in a variable
 unique_manufacturer = car_ads['manufacturer_name'].unique()
 
 #assign min and max year to functions for the year slider on the app
 min_year,max_year = int(car_ads['year_produced'].min()), int(car_ads['year_produced'].max())
+
+
+#add filter function
+def car_age(x):
+    if x<5:
+        return '<5'
+    elif x<=5 and x<10:
+        return '5-10'
+    elif x>=10 and x<20:
+        return '10-20'
+    else:
+        return '>20'
+
+#add age category column to filter scatter plot by
+car_ads['age_category'] = car_ads['age'].apply(car_age)
